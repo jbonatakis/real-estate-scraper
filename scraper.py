784689d0fdab7cb87d4b9b44f1d5ef090d06db6e
Jack Bonatakis <jackbonatakis@gmail.com>
2017-07-23 20:29:01 -0600

    Adds comments

--- scraper.py
+++ scraper.py
@@ -4,10 +4,16 @@ import datetime as dt
 import pandas as pd
 from tabulate import tabulate
 
+# Current date to be used later. Functionality to be added:
+# Read all new listings each day and generate a report on property
+# information and broker contact information. Email in usable format (csv?)
 today = dt.datetime.today().strftime(" %m/%d/%Y")
+
+#Asks user to input listing url. Will be changed to automatically select url
 loopNet = input("URL >> ")
 page = urllib.request.urlopen(loopNet)
 
+# parses given url
 soup = BeautifulSoup(page, "lxml")
 
 # print(soup.prettify())
@@ -18,6 +24,7 @@ data_table = soup.find('table', class_ = 'property-data')
 broker_name = soup.find('div', class_ = 'center-wrap')
 #broker_phone = 
 
+# Checks if listing is new (from today). If so, scrapes relevant info. Only works on loopnet, but idea is transferable
 check = 1
 while check == 1:
 	for row in date_table.findAll('td'):
@@ -28,7 +35,6 @@ while check == 1:
 			break
 		else:
 			check = 2
-			getData = 1
 
 A=[]
 B=[]
@@ -46,8 +52,7 @@ if getData == 1:
 		D.append(cells[3].find(text=True))
 
 
-
-
+# Organizes scraped info into a tabulated, easy to read data frame
 df = pd.DataFrame(A, columns = ['column1'])
 df['column2'] = B
 df['column3'] = C
